91865b78236158f5561e3498c016b3a898ef39c4
Guido Van Hoecke <guivho@gmail.com>
2019-02-17 16:43:20 +0100

    Status end lecture 84, adding a modal just to show how it is done

--- lib/pages/product_create_tab.dart
+++ lib/pages/product_create_tab.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class ProductCreateTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(child: Text('create a product'));
+    return new Center(
+      child: RaisedButton(
+        child: Text('Save'),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Center(
+                child: Text('Hi, I am a modal!'),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
